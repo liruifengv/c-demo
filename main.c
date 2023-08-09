460fe41644ed5969734c54786ef73f305ce6bd67
liruifengv <liruifeng1024@gmail.com>
2023-08-09 17:19:38 +0800

    C point

--- main.c
+++ main.c
@@ -1,29 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-  // int a = 1;       // 定义一个int型变量
-  // int *p = &a;     // 定义一个int型指针p，&a表示对a取地址，指针p的内容是a的地址
-  // printf("%p\n", &a);      // 打印a的地址
-  // printf("%p\n", p);       // 打印指针p指向的地址
-  // printf("%i\n", *p);      // 打印指针p 指向的地址的值
+  int a = 1;       // 定义一个int型变量
+  int *p = &a;     // 定义一个int型指针p，&a表示对a取地址，指针p的内容是a的地址
+  printf("a address is %p\n", &a);      // 打印a的地址
+  printf("point-p point address: %p\n", p);       // 打印指针p指向的地址
+  printf("p point address's value: %i\n", *p);      // 打印指针p 指向的地址的值
 
-  // char c[5] = {1, 2, 3, 4, 5};  // 定义一个char型数组，这里的a实质上是一个指针，指向这个数组的首元素a[0]的指针
-  // char *q = c;
-  // printf("%d\n", *q);        // 输出1 --> a[0]
-  // printf("%d\n", *(q + 1));  // 输出2 --> a[1]
-  // printf("%d\n", *q + 1);
+  printf("========================================\n");
+  printf("\n");
 
-// 常量指针是你不想别人通过你的指针去改指针指向的值
-  int a = 1;
-  int const *p = &a;
-  a++;
-  printf("*p: %i\n", *p);  
-  printf("a: %i\n", a);  
+  char c[5] = {1, 2, 3, 4, 5};  // 定义一个char型数组，这里的c实质上是一个指针，指向这个数组的首元素c[0]的指针
+  char *q = c;
+  printf("%d\n", c[0]);      // 输出1 --> c[0]
+  printf("%d\n", *q);        // 输出1 --> c[0]
+  printf("%d\n", *(q + 1));  // 输出2 --> c[1]
+  printf("%d\n", *q + 1);    // 输出2 --> c[0] + 1
 
-// 指针常量是你不想别人改你的指针里的值
-  int a = 1;
-  int * const p = &a;
-  (*p)++;
-  printf("*p: %i\n", *p);  
-  printf("a: %i\n", a);  
+  printf("========================================\n");
+  printf("\n");
+
+  int dd = 1;
+  int const *pdd = &dd; // const 修饰 int，表示指针指向的值不能改，但是指针指向的地址可以改
+  dd++;
+  // (*pdd)++;  // error: read-only variable is not assignable
+  int other = 888;
+  pdd = &other; // 可以改指针指向的地址
+  printf("&dd: %p\n", &dd);
+  printf("pdd: %p\n", pdd);
+  printf("*pdd: %i\n", *pdd);
+  printf("dd: %i\n", dd);
+
+  printf("========================================\n");
+  printf("\n");
+
+  int ee = 1;
+  int* const r = &ee; // const 修饰 int*，表示指针指向的地址不能改，但是指针指向的值可以改
+  (*r)++; // 可以修改指针指向的值
+  int other2 = 888;
+  // r = &other2; // error: assignment of read-only variable 'r' 不能改指针指向的地址
+  printf("*r: %i\n", *r);  
+  printf("ee: %i\n", ee);  
 }
---
+++ mem.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 释放内存
+void myFree(int** ppMem) {
+  printf("ppMem: %p\n", ppMem);
+  if (ppMem == NULL) {
+    return;
+  }
+  if (*ppMem != NULL) {
+    free(*ppMem);
+    *ppMem = NULL;
+  }
+}
+
+int main(void) {
+  int* p = malloc(sizeof(int));
+  *p = 12;
+  printf("p: %p\n", p);
+  printf("*p: %d\n", *p); // 12
+  printf("========================================\n");
+  printf("\n");
+
+
+  int* mem = malloc(sizeof(int));
+  *mem = 555;
+  printf("%p, %d, %p\n", mem, *mem, &mem);
+  // 释放二级指针
+  myFree(&mem);
+
+  printf("%p\n", mem);
+  // printf("%p, %d\n", mem, *mem /** use after free */);
+
+  printf("========================================\n");
+  printf("\n");
+
+  // 使用 malloc 来创建数组
+  int* arr = (int*) malloc(sizeof(int) * 10);
+
+  for (int i = 0; i < 10; i++) {
+    arr[i] = i * 5;
+  }
+
+  printf("arr: %p\n", arr);
+  printf("arr[1]: %d\n", arr[1]);
+  // 循环 arr 
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", arr[i]);
+  }
+
+  // 可以通过变量来创建动态长度的数组
+  // int* arr2 = (int*) malloc(n * sizeof(int));
+
+
+  printf("========================================\n");
+  printf("\n");
+  
+  // strcpy()将字符串“abc”拷贝放入这段内存，完成了这段内存的初始化。
+  char* str = malloc(4);
+  strcpy(str, "abc");
+  printf("%s\n", str);
+}
